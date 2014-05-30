c2da4d84d54ebfd50cb1d4bf66bbe517340359ab
Eric Boehs <ericboehs@gmail.com>
2014-05-30 03:00:55 -0500

    Add env checkers; Move threads into DAEMONIZE conditional

--- lib/smartpanel-node.rb
+++ lib/smartpanel-node.rb
@@ -4,10 +4,12 @@ lib = File.expand_path '../../lib', __FILE__
 $LOAD_PATH.unshift lib unless $LOAD_PATH.include? lib
 
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
 require 'bundler/setup'
+
 require 'logger'
 require 'json'
+require 'inotify_proxy'
+
 require 'smartpanel-node/dotenv'
 require 'smartpanel-node/configuration'
 
@@ -18,18 +20,26 @@ module SmartpanelNode
     def config
       self.configuration ||= Configuration.new
     end
+
+    def env
+      ENV.fetch('RACK_ENV', 'development')
+    end
+
+    def development?; env == 'development'; end
+    def test?       ; env == 'test'       ; end
+    def production? ; env == 'production' ; end
   end
 end
 
-require 'inotify_proxy'
 require 'smartpanel-node/breaker'
 require 'smartpanel-node/breaker_shifter'
 require 'smartpanel-node/breaker_states_poller'
 
-shifter_thread = Thread.new { SmartpanelNode::BreakerShifter.new }
-poller_thread = Thread.new { SmartpanelNode::BreakerPoller.new }
 
 if ENV['DAEMONIZE'] == 'true'
+  shifter_thread = Thread.new { SmartpanelNode::BreakerShifter.new }
+  poller_thread = Thread.new { SmartpanelNode::BreakerPoller.new }
+
   shifter_thread.join
   poller_thread.join
 end
